446e64166ead83f2d6cfc92f81f47bf2288d9a16
bakkamono <112315221+bakkamono@users.noreply.github.com>
2022-08-28 16:11:10 +0200

    Update main.ml
    added a function to display formulas as strings.

--- main.ml
+++ main.ml
@@ -18,6 +18,19 @@ type clause = litteral list
 
 type fnc = clause list
 
+(*Une fonction pour l'affichage*)
+
+let rec string_of_formule f =
+    match f with
+    |Var s -> s
+    |And (p, q) ->
+    sprintf "(%s and %s)" (string_of_formule p) (string_of_formule q)
+    |Ou (p, q) ->
+    sprintf "(%s or %s)" (string_of_formule p) (string_of_formule q)
+    |Not p ->
+    sprintf "(not %s)" (string_of_formule p)
+
+
 (*
     ENTREE
     On veut montrer que theorie => conclusion
