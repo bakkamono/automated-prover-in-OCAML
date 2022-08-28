1d40d2540fcedc1b10f062e8c797fa1f3d64e621
bakkamono <112315221+bakkamono@users.noreply.github.com>
2022-08-28 16:00:07 +0200

    Update conversion.ml
    added function flatten_or which writes expressions with only 'or's in an easier to read way, and a function or_distribution, which takes two fnc formulas p and q (their type is prop but they are written in normal conjunctive form) and commpute 'p or q'. These functions make it possible for line 76 to work.

--- conversion.ml
+++ conversion.ml
@@ -21,6 +21,45 @@ type fnc = clause list
     Pour cela, on utilise une table de conversion qui donne des équivalences entre deux propositions
 *)
 
+let rec flatten_or p = (*p = Or (...) avec que des Or, renvoie une expression en Or(Or(...))*)
+    let rec aux p acc =
+        match p with
+        |Var n -> n::acc
+        |Not (Var n) -> (-n)::acc
+        |Or (p1, p2) -> aux p1 (aux p2 acc)
+        |_ -> failwith "ne peut pas se produire"
+    in let rec without_repeat u =
+        match u with
+        |[] -> u
+        |[x] -> u
+        |x::y::s -> if x=y then without_repeat (x::s) else x::(without_repeat (y::s))
+    in let rec flatten u =
+        match u with
+        |[n] -> if n>0 then Var n else if n<0 then Not (Var n) else failwith "mauvaise indexation"
+        |n::ns -> if n>0 then Or (flatten ns, Var n) else if n<0 then Or (flatten ns, Not (Var n)) else failwith "mauvaise indexation"
+        |_ -> failwith "ne peut pas se produire"
+    in let variables_rep = aux p []
+    in let variables = without_repeat (List.sort (fun x y -> x-y) variables_rep)
+    in flatten variables
+
+let rec or_distribution p q =
+    match p, q with
+    |Var n , Var m -> Or (p, q)
+    |Var n , Not (Var m) -> Or (p, q)
+    |Var n , Or (q1, q2) -> flatten_or (Or (p, q))
+    |Var n , And (q1, q2) -> And (or_distribution (Var n) q1, or_distribution (Var n) q2)
+
+    |Not (Var n), Not (Var m) -> Or (p, q)
+    |Not (Var n), Or (q1, q2) -> flatten_or (Or (p, q))
+    |Not (Var n), And (q1, q2) -> And (or_distribution (Not (Var n)) q1, or_distribution (Not (Var n)) q2)
+
+    |Or (p1, p2), Or (q1, q2) -> flatten_or (Or (p, q))
+    |Or (p1, p2), And (q1, q2) -> or_distribution p1 (or_distribution p2 q)
+
+    |And (p1, p2), And (q1, q2) -> And (And (And (Or (p1, q1), Or (p1, q2)), Or (p2, q1)), Or (p2, q2))
+
+    |_ -> or_distribution q p
+
 let rec extract_disj f = (*donne la liste de disjonctions d'une formule en FNC*)
     let rec aux f acc =
         match f with
